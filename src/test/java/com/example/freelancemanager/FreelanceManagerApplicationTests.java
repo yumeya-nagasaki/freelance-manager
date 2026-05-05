2b196ecc67c99866318e6775e3b9cb6508bf9c63
Setsura Kawabata <kawabata@yumeya.nagasaki.jp>
2026-05-05 23:13:44 +0900

    chore: create spring boot project

---
+++ src/test/java/com/example/freelancemanager/FreelanceManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.freelancemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelanceManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
